ec29e6e64b69f81a4878e174998a06b56b6c313e
sarangs <sarangi_sunayana@shure.de>
2018-07-03 16:31:26 +0200

    Add support for connection string via an env variable

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 
+const winston = require('winston');
 const mongoose = require('mongoose');
 const microphones = require('./routes/microphones');
 const express = require('express');
 const app = express();
 
+require('./startup/db')();
+/*
 mongoose.connect('mongodb://localhost/microphones')
     .then(() => console.log('Connected to MongoDB Filter'))
     .catch(err => console.error('could not connect to MongoDB...', err));
+    */
 
 app.use(express.json());
 app.use('/api/microphones', microphones);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+//app.listen(port, () => console.log(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+module.exports = server;
