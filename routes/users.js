06672f51e46a76eb9eeeb586963a88ecb3f28934
sarangs <sarangi_sunayana@shure.de>
2019-03-08 16:38:14 +0100

    user authentication development

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 
 const {User} = require('../models/user'); 
 const bcrypt = require('bcryptjs');
+
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -38,5 +39,28 @@ router.post('/register', async (req, res) => {
     }
   });
 
+  //POST /api/users/authenticate login/authenticate an user
 
+  router.post('/authenticate', async (req, res) => {
+    const credentials = auth(req);
+    
+    if (!credentials) {
+        res.status(400).send('Invalid Request !');
+    } else {
+        const email = credentials.name;
+        const password = credentials.pass;
+        const user = await User.findOne({email: email});
+        if (!user) {
+            return res.status(404).send('No such user found');
+        } else {
+            const hashed_password = user.hashed_password;
+            if (bcrypt.compareSync(password, hashed_password)) {
+                res.send(user);
+            } else {
+                return res.status(401).send('Invalid Credentials !');
+            }
+        }
+    }
+
+  });
 module.exports = router;
