34089bd6dc4f6ddbcc23350f62babc51d4411839
sarangs <sarangi_sunayana@shure.de>
2019-04-25 11:48:40 +0200

    returning picking list itemised and delivery status as response to put request

--- routes/deliveries.js
+++ routes/deliveries.js
@@ -78,7 +78,7 @@ router.put('/issue/:delivery', async (req, res) => {
     
         if (!delivery) return res.status(404).send('The delivery with the given delivery number was not found.');
       
-      res.send(delivery.picking_list_itemised, delivery.status);
+      res.send(delivery.status);
   });
 
 module.exports = router;
