85f36bd6db19789c060d242360839427498cbcb5
sarangs <sarangi_sunayana@shure.de>
2019-05-01 15:10:09 +0200

    fixing put request

--- routes/deliveries.js
+++ routes/deliveries.js
@@ -78,7 +78,7 @@ router.put('/issue/:delivery', async (req, res) => {
     
     if (!delivery) return res.status(404).send('The delivery with the given delivery number was not found.');
       
-    res.status(200).json({picking_list_itemised: delivery.picking_list_itemised, status: delivery.status});
+    res.send({picking_list_itemised: delivery.picking_list_itemised, status: delivery.status});
       
   });
 
